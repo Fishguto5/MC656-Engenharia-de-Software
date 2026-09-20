ed176c3d3467a2b1b106cd76f486ad93cc61f8a8
Felipe Silva <204706@dac.unicamp.br>
2026-09-20 15:13:22 -0300

    ci: adicionar `@eslint/js`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
 import stylistic from '@stylistic/eslint-plugin'
+import js from '@eslint/js'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-
+  js.configs.recommended,
   stylistic.configs.recommended,
+
+  // @typescript-eslint suggest disabling this rule: https://typescript-eslint.io/troubleshooting/faqs/eslint/
+  { files: ['**/*.{ts,tsx,mts,cts}'], rules: { 'no-undef': 'off' } },
 
   globalIgnores([
     '.next/**',
